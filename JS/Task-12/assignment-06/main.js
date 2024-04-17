dad05b27e3fa76b03a05cd615ee998af33523a74
nadag1885 <nadag1885@gmail.com>
2024-04-18 01:53:12 +0200

    js

---
+++ JS/Task-12/assignment-06/main.js
@@ -0,0 +1,9 @@
+// method 1
+// using single line commints
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+
+// method 2
+// using multi-line commints
+/* console.log("Iam In Console");
+document.write("Iam In Page"); */
